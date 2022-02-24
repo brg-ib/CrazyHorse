6b67eddaf68f427665d9da678476df4cdfc459ca
ihcen <ihcen@me.com>
2022-02-24 15:11:50 +0100

    Initial commit Java Project

---
+++ src/views/MenuView.java
@@ -0,0 +1,27 @@
+package views;
+
+
+public class MenuView {
+
+
+    public MenuView() {
+
+    }
+
+    public void bonjour() {
+        System.out.println("Bonjour !");
+    }
+
+    String festivalName;
+
+    String sceneName;
+    public void choix() {
+        System.out.println("Quel action souhaitez-vous faire ?");
+        System.out.println("1 Cr�er un festival");
+        System.out.println("2 Afficher les festivals");
+        System.out.println("3 Ajouter une scene � un festival");
+        System.out.println("4 Enlever une sc�ne d'un festival");
+        System.out.println("5 Enlever un festival");
+        System.out.println("6 Modifier le nom du festival");
+    }
+}
