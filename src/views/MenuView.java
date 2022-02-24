23ce0e2842bf951e8c1e3381c5e63f51470d206c
ihcen <ihcen@me.com>
2022-02-24 15:33:22 +0100

    Added Models

--- src/views/MenuView.java
+++ src/views/MenuView.java
@@ -17,9 +17,9 @@ public class MenuView {
     String sceneName;
     public void choix() {
         System.out.println("Quel action souhaitez-vous faire ?");
-        System.out.println("1 Cr�er un festival");
+        System.out.println("1 Créer un festival");
         System.out.println("2 Afficher les festivals");
-        System.out.println("3 Ajouter une scene � un festival");
+        System.out.println("3 Ajouter une scene à un festival");
         System.out.println("4 Enlever une sc�ne d'un festival");
         System.out.println("5 Enlever un festival");
         System.out.println("6 Modifier le nom du festival");
